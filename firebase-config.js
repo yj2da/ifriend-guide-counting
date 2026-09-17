d771e4a925709358ea348b4f182f294c06729c4e
yj2da <oyjcat@naver.com>
2026-09-17 12:19:38 +0900

    feat: Firebase Realtime Database 공식 URL 기본 연동

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,7 @@
 /* ========================================================
    아이프렌드 27기 - FIREBASE 실시간 동기화 설정
-   (구글 콘솔 https://console.firebase.google.com 에서 생성)
    ======================================================== */
 
-// Firebase Realtime Database URL
-// 예: https://[프로젝트ID]-default-rtdb.firebaseio.com
 window.FIREBASE_CONFIG = {
-  // 여기에 생성하신 Firebase Realtime Database URL을 입력하시면 모든 기기에서 실시간 동기화됩니다.
-  // (웹 화면의 우측 상단 ⚙️ 버튼을 눌러서 직접 입력 및 변경하실 수도 있습니다.)
-  databaseURL: ""
+  databaseURL: "https://ifriend-guide-counting-default-rtdb.asia-southeast1.firebasedatabase.app"
 };
